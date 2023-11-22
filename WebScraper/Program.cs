c9d43c32381af40fc7898fc5c32987eb0d9013c0
Minseo <kimminseo0112@gmail.com>
2023-11-22 23:22:51 +0900

    web scraper update; begin work on LLM integration

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,42 +1,81 @@
-﻿using System.Text;
+﻿using System.Net.Http.Headers;
+using System.Text;
+using HtmlAgilityPack;
 
 namespace WebScraper;
 
-public class Program
+public partial class Program
 {
-    static Program()
-    {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-    }
+    private static readonly Uri LlmServer = new("http://localhost:18888/v1/chat/completions");
 
     public static void Main(string[] args)
     {
-        string url = $"https://news.naver.com/main/ranking/popularDay.naver";
-        string text = string.Empty;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string url = $"https://n.news.naver.com/article/055/0001108114";
 
         Task.Run(async () =>
         {
-            text = await GetContent(url);
+            var doc = await new HtmlWeb().LoadFromWebAsync(url);
 
-            string output = Path.Join(Directory.GetCurrentDirectory(), "test.txt");
-            using StreamWriter writer = new(output, Encoding.UTF8, new FileStreamOptions()
+            string str = StripHtml(doc.DocumentNode.SelectSingleNode($"//*[@id=\"dic_area\"]"));
+
+            using HttpClient client = new();
+            HttpRequestMessage request = new()
             {
-                Access = FileAccess.ReadWrite,
-                Mode = FileMode.OpenOrCreate,
-                Share = FileShare.Read
-            });
+                Method = HttpMethod.Post,
+                RequestUri = LlmServer,
+                Content = new StringContent(GeneratePrompt("todo"))
+            };
 
-            await writer.WriteAsync(text);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            string llmOutput = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(llmOutput);
         }).Wait();
     }
 
-    private static async Task<string> GetContent(string url)
+    public static string GeneratePrompt(string text)
     {
-        using HttpClient client = new();
-        using HttpResponseMessage response = await client.GetAsync(url);
-        
-        using StreamReader sr = new(await response.Content.ReadAsStreamAsync(), Encoding.GetEncoding("euc-kr"), false);
+        return "todo";
+    }
 
-        return await sr.ReadToEndAsync();
+    /// <summary>
+    /// Strips HTML node of special elements and returns in plaintext
+    /// </summary>
+    /// <param name="parent">HTML node to start from</param>
+    /// <returns>Text only body of <see cref="HtmlNode"/></returns>
+    public static string StripHtml(HtmlNode parent)
+    {
+        //create temp string builder
+        StringBuilder sb = new();
+
+        //loop through children
+        foreach (var node in parent.ChildNodes)
+        {
+            //append text if the node is just text
+            if (node.NodeType == HtmlNodeType.Text)
+            {
+                sb.Append(node.InnerText.Trim());
+
+                continue;
+            }
+
+            //append special characters and certain elements
+            sb.Append(node.Name switch
+            {
+                "span" when node.HasClass("data-lang") => node.InnerText.Trim(),
+                "strong" => node.InnerText.Trim(),
+                "br" => '\n',
+                _ => string.Empty
+            });
+        }
+
+        //trim and return stripped document
+        return sb.ToString().Replace("\n\n", "\n").Trim();
     }
 }
